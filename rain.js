7e62eafacd113e1c787dd3d6ce93d8044707ba98
coderShreyash <69112238+coderShreyash@users.noreply.github.com>
2020-09-16 15:40:56 +0530

    Bat
    Hello

---
+++ rain.js
@@ -0,0 +1,22 @@
+class d{
+    constructor(x,y,radius) {
+      var options = {
+        'restitution':0.8,
+        'friction':0.4,
+        'density':1.0
+      }
+      this.body = Bodies.circle(x,y,radius,options);
+      this.radius = radius;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      
+     
+      fill("blue");
+      ellipseMode(RADIUS);
+      ellipse(pos.x,pos.y,this.radius,this.radius);
+    }
+  };
+ 
